656e37c3a1266b6628d4ce0508edae7ea17b531f
knb729 <78786816+knb729@users.noreply.github.com>
2021-11-03 11:03:30 +0900

    1102 1102

--- src/index.js
+++ src/index.js
@@ -36,7 +36,41 @@
 // val4.address="kanagawa";
 // console.log(val4);
 
-const val5 = ["dog", "cat"];
-val5[0] = "bard";
-val5.push("add");
-console.log(val5);
+// const val5 = ["dog", "cat"];
+// val5[0] = "bard";
+// val5.push("add");
+// console.log(val5);
+
+// //デフォルト値
+// const sayHello = (name = "GUEST") => console.log(`こんにちは！${name}さん`);
+
+// sayHello("knb");
+
+// //配列の展開
+// const arr1 = [1,2];
+// console.log(arr1);
+// console.log(...arr1);
+
+// const sumfunc = (num1,num2) => console.log(num1+num2);
+
+// //sumfunc(arr1[0],arr1[1]);
+
+// sumfunc(...arr1);
+
+//まとめる
+
+// const arr2 = [1,2,3,4,5];
+
+// const [num1,num2,...arr3] = arr2;
+
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// //配列のコピー
+// const arr4 = [10,20];
+// const arr5 = [30,40];
+
+// const arr6 = [...arr4,...arr5];
+
+// console.log(arr6);
